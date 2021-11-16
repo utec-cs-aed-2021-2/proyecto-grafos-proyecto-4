c4c49b74f00c357bf3d428c04156174a325d35f9
paoloav26 <86452704+paoloav26@users.noreply.github.com>
2021-11-15 22:05:35 -0500

    Commit graph getVertex

--- Graph/graph.h
+++ Graph/graph.h
@@ -2,6 +2,7 @@
 #define GRAPH_H
 
 #include <list>
+#include <stack>
 #include <vector>
 #include <unordered_map>
 #include <iostream>
@@ -20,39 +21,43 @@ class Graph;
 
 template<typename TV, typename TE>
 struct Edge {
-    Vertex<TV, TE>* vertexes[2];
-    TE weight;
+	Vertex<TV, TE>* vertexes[2];
+	TE weight;
 };
 
 template<typename TV, typename TE>
 struct Vertex {
-    TV data;
-    std::list<Edge<TV, TE>*> edges;
+	TV data;
+	std::list<Edge<TV, TE>*> edges;
 };
 
 template<typename TV, typename TE>
 class Graph{
 protected:
-    int n_edges=0;
-    int n_vertex=0;
-    std::unordered_map<string, Vertex<TV, TE>*>  vertexes;
-
+	int n_edges=0;
+	int n_vertex=0;  
+	std::unordered_map<string, Vertex<TV, TE>*>  vertexes;
+    
 public:
-    virtual bool insertVertex(string id, TV vertex) = 0;
-    virtual bool createEdge(string id1, string id2, TE w) = 0;
-    virtual bool deleteVertex(string id) = 0;
-    virtual bool deleteEdge(string start, string end) = 0;
-    virtual TE &operator()(string start, string end)= 0;
-    virtual float density() = 0;
-    virtual bool isDense(float threshold = 0.5) = 0;
-    virtual bool isConnected()= 0;
-    virtual bool isStronglyConnected() = 0;
-    virtual bool empty() = 0;
-    virtual void clear()= 0;
+	virtual bool insertVertex(string id, TV vertex) = 0;
+	virtual bool createEdge(string id1, string id2, TE w) = 0;
+	virtual bool deleteVertex(string id) = 0;
+	virtual bool deleteEdge(string start, string end) = 0;
+	virtual TE operator()(string start, string end)= 0;  
+	virtual float density() = 0;
+	virtual bool isDense(float threshold = 0.5) = 0;
+	virtual bool isConnected()= 0;
+	virtual bool isStronglyConnected() = 0;
+	virtual bool empty() = 0;
+	virtual void clear() = 0;
+		
+	virtual void displayVertex(string id)= 0;
+	virtual bool findById(string id) = 0;
+	virtual void display() = 0;
 
-    virtual void displayVertex(string id)= 0;
-    virtual bool findById(string id) = 0;
-    virtual void display() = 0;
+	Vertex<TV, TE>* getVertex(string id){
+		return this->vertexes[id];
+	}
 };
 
 #endif
