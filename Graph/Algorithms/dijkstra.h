691e0dc5709951842b90e43a3d326805d07a7d8d
paoloav26 <86452704+paoloav26@users.noreply.github.com>
2021-12-16 22:56:37 -0500

    dijkstra commit 2

--- Graph/Algorithms/dijkstra.h
+++ Graph/Algorithms/dijkstra.h
@@ -53,7 +53,6 @@ string dijkstra(DirectedGraph<TV,TE>* grafo, string id_partida, string id_llegad
     while(!distances.empty()){
         Vertex<TV,TE>* minima = minDistance(distances,grafo);
         visited[grafo->find_id(minima)] = true;
-        cout<<"ID nodo actual: "<<grafo->find_id(minima)<<endl;
 
         auto temp_edges = grafo->getEdges(minima);
         for(auto it=temp_edges.begin();it!=temp_edges.end();it++){
@@ -71,10 +70,64 @@ string dijkstra(DirectedGraph<TV,TE>* grafo, string id_partida, string id_llegad
     //retrono del resultado en un string
     string temp_text = id_llegada;
     string solution="";
-    cout<<"Nodos que fueron visitados: "<<endl;
-    for(auto it=parents.begin();it!=parents.end();it++){
-        cout<<(*it).first<<" "<<(*it).second<<endl;
+
+    solution +=id_llegada;
+    solution +=" ";
+    while(temp_text != id_partida){
+        solution +=parents[temp_text];
+        solution +=" ";
+        temp_text = parents[temp_text];
     }
+    return solution;
+}
+
+template<typename TV,typename TE>
+Vertex<TV,TE>* minDistance(priority_queue<pair<TV,int>,vector<pair<TV,int>>,decltype(priorizar_distancia<TV>)> &distances,UnDirectedGraph<TV,TE>* grafo){
+    Vertex<TV, TE>* temp = grafo->getVertex((distances.top()).first);
+    distances.pop();
+    return temp;
+}
+
+template <typename TV, typename TE>
+string dijkstra(UnDirectedGraph<TV,TE>* grafo, string id_partida, string id_llegada){
+    int IFN=100000;
+    priority_queue<pair<TV,int>,vector<pair<TV,int>>,decltype(priorizar_distancia<TV>)> distances(priorizar_distancia<TV>);
+    unordered_map<TV,bool> visited; //vertices ya visitados
+    unordered_map<TV,int> distance; //distancias entre vertices
+    unordered_map<TV,TV> parents; // padres
+    
+
+    //algoritmo
+    
+    for(int i=0;i<grafo->getNumberVertexes();i++){
+        visited[grafo->getAllId()[i]]=false;
+        distance[grafo->getAllId()[i]]=IFN;
+    }
+
+    distance[id_partida]=0;
+    pair<TV,int> temp_pair={id_partida,0};
+    distances.push(temp_pair);
+
+    while(!distances.empty()){
+        Vertex<TV,TE>* minima = minDistance(distances,grafo);
+        visited[grafo->find_id(minima)] = true;
+
+        auto temp_edges = grafo->getEdges(minima);
+        for(auto it=temp_edges.begin();it!=temp_edges.end();it++){
+            if(!visited[grafo->find_id((*it)->vertexes[1])]){
+                if(distance[grafo->find_id((*it)->vertexes[1])] > distance[grafo->find_id((*it)->vertexes[0])] + (*it)->weight){
+                    distance[grafo->find_id((*it)->vertexes[1])] = distance[grafo->find_id((*it)->vertexes[0])] + (*it)->weight;
+                    parents[grafo->find_id((*it)->vertexes[1])] = grafo->find_id((*it)->vertexes[0]);
+                    temp_pair = {grafo->find_id((*it)->vertexes[1]),(*it)->weight};
+                    distances.push(temp_pair);
+                }
+            }
+        }
+    }
+
+    //retrono del resultado en un string
+    string temp_text = id_llegada;
+    string solution="";
 
     solution +=id_llegada;
     solution +=" ";
